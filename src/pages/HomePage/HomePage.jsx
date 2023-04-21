e36eed54643df200211d8f985db6f5516855213d
Sally-Kam <skam2000@gmail.com>
2023-04-21 01:20:08 -0400

    update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,6 +10,7 @@ export default function HomePage() {
   <div className="text-center">
     <img className="text-center object-center h-50 w-60"src="https://i.imgur.com/NklP0nn.png"></img>
   </div>
+    <div className="p-3 w-full max-w-lg">Welcome to Canvas MarketPlace where you can find an extensive collection of cool canvas pieces to add a touch of personality to any room. Whether you're looking for artwork to brighten up your home, office, or any other space, we've got you covered. Our canvas pieces range from minimalist designs to bold and colorful prints that can transform any dull wall into a focal point. We offer a variety of art to suit your needs, and our user-friendly website makes it easy to browse and purchase your favorite pieces from the comfort of your own home. Shop with us today and add some artistic flair to your space!</div>
     <div className="text-xl text-center">
     <Link className="outline text-center hover:outline-double" to="/products">View All Products</Link>
     </div>
